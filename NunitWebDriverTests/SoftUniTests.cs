7b4f8d23664bdce595bf8adb69086c265d35b9d0
miglenamag <miglena_mag@yahoo.com>
2022-05-17 23:22:33 +0300

    Add project files.

---
+++ NunitWebDriverTests/SoftUniTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NunitWebDriverTests
+{
+    public class SoftUniTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_AssertMainTitle()
+        {
+
+            // Arrange
+            var driver = new ChromeDriver();
+
+
+        //Act
+        Driver.Url = "https://softuni.bg";
+            string expectedTitle = "Обучение по програмиране - Софтуерен университет";
+            Assert.AreEqual()
+
+            driver.FindElement()
+
+        }
+    }
+}
---
+++ WebDriverTestsDemo/WikipediaTest.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// Create new Chrome browser instance
+var driver = new ChromeDriver();
+
+// Navigate to wikipedia
+driver.Url = "https://wikipedia.org";
+
+// Locate search by Id
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+// Click on element
+searchField.Click();
+
+// Fill QA and press Enter button
+searchField.SendKeys("QA" + Keys.Enter);
+
+System.Console.WriteLine("Title after search: " + driver.Title);
+
+//Close browser
+driver.Quit();
