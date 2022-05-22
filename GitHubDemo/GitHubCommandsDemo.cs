a49e5dbe62e1d121dca9db89a057fda042f979fd
miglenamag <miglena_mag@yahoo.com>
2022-05-22 18:38:47 +0300

    Added new WebDriverDemoGitHub

---
+++ GitHubDemo/GitHubCommandsDemo.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+// Create New Chrome Browser Instance
+var driver = new ChromeDriver();
+
+// Navigate to GitHub
+// driver.Url = "https://github.com/";
+driver.Navigate().GoToUrl("https://github.com/");
+
+//Print main page title
+//Console.WriteLine($" The main page title is {driver.Title}");
+Console.WriteLine("MAIN PAGE TITLE: " + driver.Title);
+Console.WriteLine();
+
+// Locate Search field by XPath
+var searchField = driver.FindElement(By.XPath("/html/body/div[1]/header/div/div[2]/div[2]/div[1]/div/div/form/label/input[1]")
+    );
+
+
+// Click on element
+searchField.Click();
+
+//Type search item and press enter 
+//searchField.SendKeys("WebDriverTestsDemo" + Keys.Enter);
+searchField.SendKeys("WebDriverTestsDemo");
+searchField.SendKeys(Keys.Enter);
+Console.Write($"Page title after search: {driver.Title}");
+Console.WriteLine();
+
+
+// Close browser
+driver.Quit();
+
--- NunitWebDriverTests/SoftUniTests.cs
+++ NunitWebDriverTests/SoftUniTests.cs
@@ -19,12 +19,35 @@ namespace NunitWebDriverTests
             var driver = new ChromeDriver();
 
 
-        //Act
-        Driver.Url = "https://softuni.bg";
+            //Act
+            driver.Url = "https://softuni.bg";
             string expectedTitle = "Обучение по програмиране - Софтуерен университет";
-            Assert.AreEqual()
 
-            driver.FindElement()
+            //Assert
+            Assert.That(driver.Title, Is.EqualTo(expectedTitle));
+
+
+            driver.Quit();
+
+        }
+        public void Test_AssertAboutUsTitle()
+        {
+
+            // Arrange
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+
+            //Act
+            driver.Url = "https://softuni.bg";
+            var ZaNasElement = driver.FindElement(By.CssSelector("header-nav > div.toggle-nav.toggle-holder.toggle-active > ul > li:nth-child(1) > a > span"));
+            string expectedTitle = "За нас - Софтуерен университет";
+
+            //Assert
+            Assert.That(driver.Title, Is.EqualTo(expectedTitle));
+
+
+            driver.Quit();
 
         }
     }
